9008abcfa91a32afb7af2bbf564487dde5661b4c
AntoineRb <antoine.robert7700@gmail.com>
2022-01-08 15:04:26 +0100

    Fix protocol error

--- js/modules/callApi.js
+++ js/modules/callApi.js
@@ -47,7 +47,7 @@ export function setNewCity( cityToDisplay ) {
 export async function weatherInfo() {
 
     if ( !userIsGeolocate ) {
-        url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${cityApiKey}&units=${units}`;
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${cityApiKey}&units=${units}`;
     } else if ( userIsGeolocate ) {
         url = `https://api.openweathermap.org/data/2.5/weather?lat=${userLatitude}&lon=${userLongitude}&appid=${geolocateApiKey}&units=${units}`
     }
